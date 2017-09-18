0190eeaa7a85373946bdefd6cde13549898caf49
Jakub Stefanski <code@jakubstefanski.com>
2017-09-18 18:55:36 +0200

    Change background color to black

--- config.def.h
+++ config.def.h
@@ -86,7 +86,7 @@ static unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	"#282828",
+	"#000000",
 	"#cc241d",
 	"#98971a",
 	"#d79921",
